57c62efd35781fccbd0d0d496429efc3ccd6d49d
Tamim Ahasan Rijon <tamimahsan.ar@gmail.com>
2024-09-05 02:38:58 +0600

    up

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,9 +1,29 @@
 import Image from "./Image";
+import WorkSample from "./WorkSample";
 
 export default function App() {
+  const workData = [
+    {
+      year: "1933-1937",
+      experience: "Actress",
+      institution: "Vienna, Austria",
+    },
+    {
+      year: "1940-1945",
+      experience: "Inventor",
+      institution: "Hollywood, California",
+    },
+    {
+      year: "1945-1950",
+      experience: "Scientist",
+      institution: "New York, New York",
+    },
+  ];
   return (
     <div>
-      <Image />
+      {workData.map((work) => (
+        <WorkSample work={work} />
+      ))}
     </div>
   );
 }
