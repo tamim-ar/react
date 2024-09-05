6729d880bdd0788471fa273279eb4b37bd911d68
Tamim Ahasan Rijon <tamimahsan.ar@gmail.com>
2024-09-05 15:52:56 +0600

    up

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,11 +1,12 @@
 import Image from "./Image";
 import WorkSample from "./WorkSample";
 import Counter from "./Counter";
+import LoadUser from "./LoadUser";
 
 export default function App() {
   return (
     <div>
-      <Counter />
+      <LoadUser />
     </div>
   );
 }
