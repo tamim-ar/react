d1cb5b26ba3ceab592329c0c905fed8b1bb5977a
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-17 02:48:30 +0600

    users-fetch

--- load-users-from-api/src/App.jsx
+++ load-users-from-api/src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import User from './User'
+import React, { useState, useEffect } from "react";
+import User from "./User";
 
 export default function App() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => setUser(data.users));
+  }, []);
+
   return (
-    <div className='flex gap-3 flex-wrap justify-center'>
+    <div className="flex gap-3 flex-wrap justify-center">
       <User />
     </div>
-  )
+  );
 }
