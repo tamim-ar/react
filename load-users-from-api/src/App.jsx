2a70d495a4da2ae75d1bed2fb57850cb9655b06e
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-17 02:24:33 +0600

    User Create

--- load-users-from-api/src/App.jsx
+++ load-users-from-api/src/App.jsx
@@ -1,12 +1,10 @@
+import React from 'react'
+import User from './User'
+
 export default function App() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Users</h1>
-      <ul>
-        <li className="mb-2">User 1</li>
-        <li className="mb-2">User 2</li>
-        <li className="mb-2">User 3</li>
-      </ul>
+    <div className='flex gap-3 flex-wrap justify-center'>
+      <User />
     </div>
-  );
+  )
 }
