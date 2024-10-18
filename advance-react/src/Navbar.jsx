e4c89d5ba7128cac0755f7ac39b6b836353f1680
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-19 03:10:21 +0600

    navlink

--- advance-react/src/Navbar.jsx
+++ advance-react/src/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div>
-      <a href="/">Home</a>
-      <a href="login">Login</a>
-      <a href="registration">Register</a>
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "text-danger" : "")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/login"
+        className={({ isActive }) => (isActive ? "text-danger" : "")}
+      >
+        LogIn
+      </NavLink>
+      <NavLink
+        to="/registration"
+        className={({ isActive }) => (isActive ? "text-danger" : "")}
+      >
+        Registration
+      </NavLink>
     </div>
   );
 }
